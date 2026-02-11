a78f1f0ab72d9665e327a37ec3361ee38fcffff2
jovlasin <196852052+jovlasin@users.noreply.github.com>
2026-02-10 20:06:22 -0500

    feat: Initialize main game loop

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,10 +5,15 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
         JFrame window = new JFrame();
+        GameHandler gh = new GameHandler();
+
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("NanoCraft");
+        window.add(gh);
+        window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+        gh.startGame();
     }
 }
