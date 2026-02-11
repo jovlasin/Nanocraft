86db8a90269139d021ffd327d0d1a2ffc39402dd
jovlasin <196852052+jovlasin@users.noreply.github.com>
2026-02-11 11:13:33 -0500

    fix: Fixed errors and resolved issues regarding KeyListener, img paths, and player being null

--- src/main/GameHandler.java
+++ src/main/GameHandler.java
@@ -2,23 +2,33 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JPanel;
+
+import entity.Player;
+import map.TileHandler;
 
 public class GameHandler extends JPanel implements Runnable {
     private final int defaultTileSize = 16; // tiles are 16x16 pngs
     private final int scale = 3;
     private final int maxScreenCol = 16; // 16 tiles wide
     private final int maxScreenRow = 12; // 12 tiles tall
-    private final int tileSize = defaultTileSize * scale; // scale tile to 48x48
-    private final int screenWidth = tileSize * maxScreenCol; // scale screen width to 768px
-    private final int screenHeight = tileSize * maxScreenRow; // scale screen height to 576 tall
+    public final int tileSize = defaultTileSize * scale; // scale tile to 48x48
+    public final int screenWidth = tileSize * maxScreenCol; // scale screen width to 768px
+    public final int screenHeight = tileSize * maxScreenRow; // scale screen height to 576 tall
     public double fps = 60; // update the game 60 times per sec
     public Thread gameThread;
+    public KeyHandler kh = new KeyHandler();
+    public Player player = new Player(this, kh);
+    public TileHandler th = new TileHandler(this);
 
     public GameHandler() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(kh);
         this.setFocusable(true);
     }
 
@@ -45,8 +55,8 @@ public class GameHandler extends JPanel implements Runnable {
             time = currentTime;
 
             while (delta >= 1) {
-                // update();
-                // repaint();
+                update();
+                repaint();
                 delta--;
                 // drawCount++;
             }
@@ -58,5 +68,18 @@ public class GameHandler extends JPanel implements Runnable {
             //     timer = 0;
             // }
         }
+    }
+
+    public void update() {
+        player.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        th.draw(g2d);
+        player.draw(g2d);
+        g2d.dispose();
     }
 }
