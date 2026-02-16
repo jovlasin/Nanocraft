289a736170b84717c07c6114fde03e29742b0016
Justin Tavarez <jtavarez@ggc.edu>
2026-02-16 16:24:22 -0500

    Refactor Game/Tile handlers to support Tiled JSON maps with multiple layers

--- src/core/GameHandler.java
+++ src/core/GameHandler.java
@@ -1,86 +1,72 @@
 package core;
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-
 import javax.swing.JPanel;
-
 import entity.Player;
 import input.KeyHandler;
 import tile.TileHandler;
-
 public class GameHandler extends JPanel implements Runnable {
-    private final int defaultTileSize = 16; // tiles are 16x16 pngs
-    private final int scale = 3;
-    private final int maxScreenCol = 16; // 16 tiles wide
-    private final int maxScreenRow = 12; // 12 tiles tall
-    public final int tileSize = defaultTileSize * scale; // scale tile to 48x48
-    public final int screenWidth = tileSize * maxScreenCol; // scale screen width to 768px
-    public final int screenHeight = tileSize * maxScreenRow; // scale screen height to 576 tall
-    public double fps = 60; // update the game 60 times per sec
-    public Thread gameThread;
-    public KeyHandler kh = new KeyHandler();
-    public Player player = new Player(this, kh);
-    public TileHandler th = new TileHandler(this);
+private final int defaultTileSize = 16; // tiles are 16x16 pngs
+private final int scale = 3;
+private final int maxScreenCol = 16; // 16 tiles wide
+private final int maxScreenRow = 12; // 12 tiles tall
+public final int tileSize = defaultTileSize * scale; // scale tile to 48x48
+public final int screenWidth = tileSize * maxScreenCol; // scale screen width to 768px
+public final int screenHeight = tileSize * maxScreenRow; // scale screen height to 576 tall
+public double fps = 60; // update the game 60 times per sec
+public Thread gameThread;
+public KeyHandler kh = new KeyHandler();
+public Player player = new Player(this, kh);
+public TileHandler th = new TileHandler(this);
 
-    public GameHandler() {
-        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.BLACK);
-        this.setDoubleBuffered(true);
-        this.addKeyListener(kh);
-        this.setFocusable(true);
+public GameHandler() {
+this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+this.setBackground(Color.BLACK);
+this.setDoubleBuffered(true);
+this.addKeyListener(kh);
+this.setFocusable(true);
+}
+
+public void startGame() {
+gameThread = new Thread(this);
+gameThread.start();
+}
+
+@Override
+public void run() {
+double drawInterval = 1000000000.0 / fps;
+double delta = 0;
+long time = System.nanoTime();
+long currentTime;
+
+while (gameThread != null) {
+currentTime = System.nanoTime();
+delta += (currentTime - time) / drawInterval;
+time = currentTime;
+while (delta >= 1) {
+update();
+repaint();
+delta--;
     }
+}
+}
 
-    public void startGame() {
-        gameThread = new Thread(this);
-        gameThread.start();
-    }
+public void update() {
+player.update();
+}
 
-    @Override
-    public void run() {
-        double drawInterval = 1000000000.0 / fps;
-        double delta = 0;
-        long time = System.nanoTime();
-        long currentTime;
-
-        // debug
-        // long timer = 0;
-        // int drawCount = 0;
-
-        while (gameThread != null) {
-            currentTime = System.nanoTime();
-            delta += (currentTime - time) / drawInterval;
-            // timer += (currentTime - time);
-            time = currentTime;
-
-            while (delta >= 1) {
-                update();
-                repaint();
-                delta--;
-                // drawCount++;
-            }
-
-            // // debug
-            // if (timer >= 1000000000) {
-            //     System.out.println("FPS: " + drawCount);
-            //     drawCount = 0;
-            //     timer = 0;
-            // }
-        }
-    }
-
-    public void update() {
-        player.update();
-    }
-
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-
-        th.draw(g2d);
-        player.draw(g2d);
-        g2d.dispose();
+public void paintComponent(Graphics g) {
+super.paintComponent(g);
+Graphics2D g2d = (Graphics2D) g;
+for (int i = 0; i < th.getBelowPlayerLayerCount(); i++) {
+th.drawLayer(g2d, i);
+}
+player.draw(g2d);
+for (int i = th.getBelowPlayerLayerCount(); i < th.getLayerCount(); i++) {
+th.drawLayer(g2d, i);
+}
+g2d.dispose();
     }
 }
