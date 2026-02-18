13d04dbe04399c9c5bc8b33af01bce3c4afd75b0
Justin Tavarez <jtavarez@ggc.edu>
2026-02-18 17:02:08 -0500

    Code clean up and refactor

---
+++ src/tile/TiledMapModels.java
@@ -0,0 +1,40 @@
+package tile;
+
+import java.util.List;
+
+class TiledMapData {
+    int width;
+    int height;
+    List<TiledLayerData> layers;
+    List<TiledTilesetData> tilesets;
+}
+
+class TiledLayerData {
+    String name;
+    String type;
+    int[] data;
+}
+
+class TiledTilesetData {
+    int firstgid;
+    String source;
+    String image;
+    int imagewidth;
+    int imageheight;
+    int tilewidth;
+    int tileheight;
+    int tilecount;
+    int columns;
+    List<TiledTileData> tiles;
+}
+
+class TiledTileData {
+    int id;
+    String image;
+    List<TiledPropertyData> properties;
+}
+
+class TiledPropertyData {
+    String name;
+    Object value;
+}
