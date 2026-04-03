e3ea53330ae65c293795e87c8b8159ada27a8090
jovlasin <196852052+jovlasin@users.noreply.github.com>
2026-04-02 21:04:34 -0400

    feat: add simple npc ai for movement

--- src/main/java/com/nanocraft/game/entity/Entity.java
+++ src/main/java/com/nanocraft/game/entity/Entity.java
@@ -81,7 +81,40 @@ public class Entity {
         }
     }
 
+    public void setAction() {}
+
     public void update() {
+        setAction();
+
+        switch (direction) {
+            case "up":
+                worldY -= speed;
+            break;
         
+            case "down":
+                worldY += speed;
+            break;
+
+            case "left":
+                worldX -= speed;
+            break;
+
+            case "right":
+                worldX += speed;
+            break;
+        }
+        
+        spriteCounter++;
+
+        if (spriteCounter > 14) {
+            if (spriteNum == 1) {
+                spriteNum = 2;
+            }
+
+            else if (spriteNum == 2) {
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
     }
 }
