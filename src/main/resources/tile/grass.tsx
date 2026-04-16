fe6b898879cca943e6d97fd6405823d6c9662bff
jovlasin <196852052+jovlasin@users.noreply.github.com>
2026-04-16 16:14:29 -0400

    assets: Add desert map

---
+++ src/main/resources/tile/grass.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="grass" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="../nano/New version (with numbers)/002.png" width="16" height="16"/>
+ <tile id="0">
+  <properties>
+   <property name="direction" value="right"/>
+   <property name="targetCol" type="int" value="1"/>
+   <property name="targetMap" value="village.tmj"/>
+   <property name="targetRow" type="int" value="6"/>
+  </properties>
+ </tile>
+</tileset>
